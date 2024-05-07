58599f691a7a3bd64dd140c3bd6f0366d73af83a
Tony Lin <ttony.lin@mail.utoronto.ca>
2024-05-07 12:50:23 -0400

    maybe flushing will fix logging

--- stores/canyonBicycles.py
+++ stores/canyonBicycles.py
@@ -1,11 +1,10 @@
 import os
-from datetime import datetime
 import discordNotification
 
 def check(soup):
     inStockButton = soup.find(id='js-productDetailBuyButton')
     innerText = inStockButton.getText(strip=True).lower()
-    print(f'{datetime.now()} ||| Stock status: {innerText}')
+    print(f'Stock status: {innerText}', flush=True)
     if innerText == 'add to cart':
         url = os.environ.get('URL')
         message = f'THE BIKE IS IN STOCK\n{url}\n(If I\'m still spamming and Tony isn\'t available to stop me, right click me on the sidebar and mute me for now.)'

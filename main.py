345f41ec56c8882e4de5d6a82bc44b7c73d91794
Tony Lin <ttony.lin@mail.utoronto.ca>
2024-05-07 03:54:14 -0400

    env schema, dummy app for wsgi

--- main.py
+++ main.py
@@ -8,12 +8,25 @@ import canyonBicycles
     Repeatly checks the online store for stock every interval.
 '''
 def repeat(url, headers, store, interval):
-        timer = Timer(interval, repeat, (url, headers, store, interval))
-        timer.start()
+    timer = Timer(interval, repeat, (url, headers, store, interval))
+    timer.start()
 
-        request = requests.get(url, headers=headers)
-        soup = BeautifulSoup(request.text, features='html.parser')
-        store.check(soup)
+    request = requests.get(url, headers=headers)
+    soup = BeautifulSoup(request.text, features='html.parser')
+    store.check(soup)
+
+'''
+    Dummy app for port binding
+'''
+def app(environ, start_response):
+    data = b'hi\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
 
 if __name__ == '__main__':
     url = os.environ.get('URL')

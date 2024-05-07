522d5c74dff5a414664c9710db5f6ef4b475135f
Tony Lin <ttony.lin@mail.utoronto.ca>
2024-05-07 14:15:51 -0400

    keep instance awake

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ def repeat(url, headers, store, interval):
 
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, features='html.parser')
+    keepInstanceAwake()
     store.check(soup)
+
+'''
+    Keep free tier instance awake by periodically calling this function
+'''
+def keepInstanceAwake():
+    url = os.environ.get('RENDER_URL')
+    headers = {'User-Agent': os.environ.get('USER_AGENT')}
+    response = requests.get(url, headers=headers)
+    print(f'This instance\'s status code: {response.status_code}')
+
 
 def main():
     url = os.environ.get('URL')

bc6a317e8223ea4b86eab86d08dc9407185072a5
Tony Lin <ttony.lin@mail.utoronto.ca>
2024-05-07 04:10:16 -0400

    moved app to its own file

---
+++ app.py
@@ -0,0 +1,12 @@
+'''
+    Dummy app for port binding
+'''
+def app(environ, start_response):
+    data = b'hi\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
--- main.py
+++ main.py
@@ -15,19 +15,6 @@ def repeat(url, headers, store, interval):
     soup = BeautifulSoup(request.text, features='html.parser')
     store.check(soup)
 
-'''
-    Dummy app for port binding
-'''
-def app(environ, start_response):
-    data = b'hi\n'
-    status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    return iter([data])
-
 if __name__ == '__main__':
     url = os.environ.get('URL')
     headers = {'User-Agent': os.environ.get('USER_AGENT')}
